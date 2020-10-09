0f11254122433fa22b174024e619dd99c99625c4
samlin233 <43654770+samlin233@users.noreply.github.com>
2020-10-09 07:09:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import CredsForm from './components/SaveCreds';
+import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    return (
+        <main>
+            <Switch>
+		<Route path="/" component={Home} exact />
+                <Route path="/creds" component={CredsForm} />
+	        <Route component={Error} />
+            </Switch>
+	    <Navbar />
+        </main>
+    );
+};
 export default App;
