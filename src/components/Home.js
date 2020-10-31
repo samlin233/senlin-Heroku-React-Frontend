9ddc1b106c13f28c8bbce44a40afebc258cbace1
samlin233 <lin354425148@gmail.com>
2020-10-30 23:54:14 -0700

    #

--- src/components/Home.js
+++ src/components/Home.js
@@ -54,37 +54,48 @@ function Home() {
             <p className="lead mb-0"><a href="#" className="text-white font-weight-bold">Continue reading...</a></p>
           </div>
         </div>
-        <div className="row mb-2">
-          <div className="col-md-6">
-            <div className="card flex-md-row mb-4 box-shadow h-md-250">
-              <div className="card-body d-flex flex-column align-items-start">
-                <strong className="d-inline-block mb-2 text-primary">World</strong>
-                <h3 className="mb-0">
-                  <a className="text-dark" href="#">Featured post</a>
-                </h3>
-                <div className="mb-1 text-muted">Nov 12</div>
-                <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                <a href="#">Continue reading</a>
-              </div>
-              <img className="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap" />
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="card flex-md-row mb-4 box-shadow h-md-250">
-              <div className="card-body d-flex flex-column align-items-start">
-                <strong className="d-inline-block mb-2 text-success">Design</strong>
-                <h3 className="mb-0">
-                  <a className="text-dark" href="#">Post title</a>
-                </h3>
-                <div className="mb-1 text-muted">Nov 11</div>
-                <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                <a href="#">Continue reading</a>
-              </div>
-              <img className="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap" />
-            </div>
-          </div>
-        </div>
-      </div>
+        <CardGroup>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to additional
+                content.{' '}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This card has even longer content than the first to
+                show that equal height action.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardGroup>
       <main role="main" className="container">
         <div className="row">
           <div className="col-md-8 blog-main">
