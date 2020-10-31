751d0be7dc0853790227374c144d550225a51696
samlin233 <lin354425148@gmail.com>
2020-10-31 02:37:37 -0700

    #

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../style.css';
 import '../css/bootstrap.min.css';
+import 'react-bootstrap';
 import Card from 'react-bootstrap/Button';
 import CardGroup from 'react-bootstrap/Button';
 function Home() {
