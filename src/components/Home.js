e00c022f106a0b28637fd989db53abdb0218cd7e
samlin233 <lin354425148@gmail.com>
2020-11-03 03:45:18 -0800

    add popout for loggingin

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,18 +26,27 @@ function MyVerticallyCenteredModal(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          Sign up for new account
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
+        <h4>Creat New Account</h4>
         <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
+          User Name:
         </p>
+        <p>
+        <FormControl type="text" placeholder="Enter Your User Name" className="mr-sm-2" />
+        </p>        
+        <p>
+          Pass Word:
+        </p>
+        <p>
+        <FormControl type="text" placeholder="Enter Your Password" className="mr-sm-2" />
+        </p>
+
       </Modal.Body>
       <Modal.Footer>
+        <Button variant="outline-light">Sign Up</Button>
         <Button onClick={props.onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
