dd734fbb0408c864f0a865441e917dd459a22be2
samlin233 <lin354425148@gmail.com>
2020-11-06 10:05:46 -0800

    Testing new home page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import CredsForm from './components/SaveCreds';
-import index from './index'
+import Index from './components/index'
 
 function App() { 
     return (
         <main>
             <Switch>
-                <Route path="/" component={index} exact />
+                <Route path="/" component={Index} exact />
                 {/*<Route path="/creds" component={CredsForm} />*/}
                 <Route component={Error} />
             </Switch>
